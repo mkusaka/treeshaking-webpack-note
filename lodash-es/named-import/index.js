1c76b0af8eaf9cbff76d1abc1059c92ce20ee7c8
mkusaka <hinoshita1992@gmail.com>
2020-03-06 18:41:15 +0900

    add lodash-es pattern

---
+++ lodash-es/named-import/index.js
@@ -0,0 +1,7 @@
+import { uniq } from "lodash-es";
+
+uniq([
+  { a: 1, b: 2 },
+  { a: 1, b: 2 },
+  { a: 2, b: 2 }
+]);
---
+++ lodash-es/scoped-import/index.js
@@ -0,0 +1,7 @@
+import uniq from "lodash-es/uniq";
+
+uniq([
+  { a: 1, b: 2 },
+  { a: 1, b: 2 },
+  { a: 2, b: 2 }
+]);
